34e8dd40c46cf5edbecc6b8d08004c52a8fd0a9a
kenji <kenjix12x22@gmail.com>
2021-02-09 22:26:51 +0900

    sort.py

---
+++ sort.py
@@ -0,0 +1,26 @@
+import random
+
+
+def sort(nums):
+    box = [0]
+    for i in nums:
+        if i > 0:
+            for ii in range(len(box)):
+                ii = ii + 1
+                if box[-ii] < i:
+                  box.insert(len(box) - ii + 1, i)
+                  break
+        else:
+            for ii in range(len(box)):
+                ii = ii 
+                if box[ii] > i:
+                    box.insert(ii, i)
+                    break
+    box.remove(0)
+    return box
+
+
+nums = [random.randint(-1000, 1000) for _ in
+range(1000)]
+result = sort(nums)
+print(result)
