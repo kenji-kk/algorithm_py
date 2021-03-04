fc9cc2e2e8694b24e677c74a944433c6302449c7
kenji <kenjix12x22@gmail.com>
2021-03-04 23:12:23 +0900

    数値変更

--- list1-19.py
+++ list1-19.py
@@ -1,12 +1,12 @@
 #1から12までを8をスキップして繰り返す
 #ダメなら例
-for i in range(1,15):
+for i in range(1,100):
   if i == 8:
     continue
   print(i, end='')
 print()
 
 #良い例
-for i in list(range(1, 15)) + list(range(9, 13)):
+for i in list(range(1, 100)) + list(range(9, 13)):
   print(i, end=' ')
 print()
