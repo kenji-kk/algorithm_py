a066bc789b8d404f55e2d921ccdc3e9ec1fa7cae
kenji <kenjix12x22@gmail.com>
2021-03-05 23:37:27 +0900

    リファクタリング

--- list1-19.py
+++ list1-19.py
@@ -1,12 +1,12 @@
 #1から12までを8をスキップして繰り返す
 #ダメなら例
-for i in range(1,100):
+for i in range(1,150):
   if i == 8:
     continue
   print(i, end='')
 print()
 
 #良い例
-for i in list(range(1, 100)) + list(range(9, 13)):
+for i in list(range(1, 8)) + list(range(9, 150)):
   print(i, end=' ')
 print()
