d1c9ef9b25da5029585db74604891896ce8f0359
kenji <kenjix12x22@gmail.com>
2021-03-10 23:54:34 +0900

    横棒のかける回数を二分の一に

--- list1-21.py
+++ list1-21.py
@@ -1,7 +1,7 @@
 #多重プログラミング 九九の表示
-print('-', * 35)
+print('--', * 17)
 for i in range(1,15):
   for j in range(1, 15):
     print(f'{i * j:3}', end='')
   print()
-print('-', * 35)
+print('--', * 17)
