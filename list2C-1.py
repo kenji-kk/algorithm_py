85802b4e8b9d8313f6703b50cc708de0d7d4ab56
kenji <kenjix12x22@gmail.com>
2021-03-18 23:49:50 +0900

    enumerate関数バージョンを追加

--- list2C-1.py
+++ list2C-1.py
@@ -2,3 +2,8 @@
 x = ["aaa","bbb","ccc"]
 for i in range(len(x)):
   print(f'x[{i}] = {x[i]}'')
+
+#リストの全要素をenumerate関数で走査
+x = ["aaa","bbb","ccc"]
+for i, name in enumerate(x):
+  print(f'x[{i}] = {name}')
