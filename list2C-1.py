6f5d8740a9dce54edf4545ec4e18b334dda7016d
kenji <kenjix12x22@gmail.com>
2021-04-02 23:46:47 +0900

    配列に要素を追加

--- list2C-1.py
+++ list2C-1.py
@@ -1,19 +1,19 @@
 #リストの全要素を走査（要素数を事前に取得）
-x = ["aaa","bbb","ccc"]
+x = ["aaa","bbb","ccc","ddd"]
 for i in range(len(x)):
   print(f'x[{i}] = {x[i]}'')
 
 #リストの全要素をenumerate関数で走査
-x = ["aaa","bbb","ccc"]
+x = ["aaa","bbb","ccc","ddd"]
 for i, name in enumerate(x):
   print(f'x[{i}] = {name}')
 
 #リストの全要素をenumerate関数で走査（１からカウント）
-x = ["aaa","bbb","ccc"]
+x = ["aaa","bbb","ccc","ddd"]
 for i, name in enumerate(x, i):
   print(f'{i}番目 = {name}')
 
 #リストの全要素を走査（インデックスを使わない）
-x = ["aaa","bbb","ccc"]
+x = ["aaa","bbb","ccc","ddd"]
 for i in x:
   print(i)
